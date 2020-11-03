9ea4c2c5c0553975403d10098f301bbf764d1107
Vladimir <groeter457028@gmail.com>
2020-11-03 12:07:34 +0400

    add enter

--- local/templates/bitrix24/components/bitrix/crm.deal.details/.default/result_modifier.php
+++ local/templates/bitrix24/components/bitrix/crm.deal.details/.default/result_modifier.php
@@ -5,7 +5,8 @@
  */
 // remove unwanted fields
 unset($arResult['ENTITY_FIELDS'][0]);
-if ($arResult['ENTITY_DATA']['STAGE_ID'] !== 'LOSE' && $arResult['ENTITY_DATA']['STAGE_ID'] !== 'APOLOGY')
+if ($arResult['ENTITY_DATA']['STAGE_ID'] !== 'LOSE'
+    && $arResult['ENTITY_DATA']['STAGE_ID'] !== 'APOLOGY')
 {
     unset($arResult['ENTITY_FIELDS'][21]);
 }
