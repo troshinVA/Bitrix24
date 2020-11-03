446f030fb694dfcbeca617cb5e164ce6a69ec9b1
Vladimir <groeter457028@gmail.com>
2020-11-03 10:39:42 +0400

    init commit

---
+++ local/templates/bitrix24/components/bitrix/crm.deal.details/.default/result_modifier.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @var array $arResult
+ */
+// remove unwanted fields
+unset($arResult['ENTITY_DATA']['ID']);
+if ($arResult['ENTITY_DATA']['STAGE_ID'] !== 'LOSE' && $arResult['ENTITY_DATA']['STAGE_ID'] !== 'APOLOGY')
+{
+    unset($arResult['ENTITY_DATA']['UF_CRM_FAIL_COMMENT']);
+}
+
+var_dump($arResult['ENTITY_DATA']);
+var_dump($arResult['ENTITY_DATA']['ID']);
+
+
