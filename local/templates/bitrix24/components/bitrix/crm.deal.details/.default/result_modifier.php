62048c271bdfde63105f7482869f0a17ea01e34c
Vladimir <groeter457028@gmail.com>
2020-11-03 12:06:10 +0400

    edit result_modifier ENTITY_FIELDS

--- local/templates/bitrix24/components/bitrix/crm.deal.details/.default/result_modifier.php
+++ local/templates/bitrix24/components/bitrix/crm.deal.details/.default/result_modifier.php
@@ -4,13 +4,9 @@
  * @var array $arResult
  */
 // remove unwanted fields
-unset($arResult['ENTITY_DATA']['ID']);
+unset($arResult['ENTITY_FIELDS'][0]);
 if ($arResult['ENTITY_DATA']['STAGE_ID'] !== 'LOSE' && $arResult['ENTITY_DATA']['STAGE_ID'] !== 'APOLOGY')
 {
-    unset($arResult['ENTITY_DATA']['UF_CRM_FAIL_COMMENT']);
+    unset($arResult['ENTITY_FIELDS'][21]);
 }
-//unset($arResult['ENTITY_DATA']['STAGE_ID']);
-var_dump($arResult['ENTITY_DATA']);
-var_dump($arResult['ENTITY_DATA']['ID']);
-
-
+unset($arResult['ENTITY_FIELDS'][7]);
